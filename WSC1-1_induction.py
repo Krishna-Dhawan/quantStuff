11b7317c6468d08d5ccb8b6ccbfcb3bee1bc507c
Krishna-Dhawan <dhawan.krish@gmail.com>
2023-11-24 19:00:11 +0530

    historical price performance

--- WSC1-1_induction.py
+++ WSC1-1_induction.py
@@ -5,9 +5,17 @@ os.chdir("Stock Data - Quant Task")
 csv_data = ["ADANIENT.NS.csv", "AXISBANK.NS.csv", "BHARTIARTL.NS.csv", "CIPLA.NS.csv", "HINDALCO.NS.csv",
             "HINDUNILVR.NS.csv", "INFY.NS.csv", "ITC.NS.csv", "MARUTI.NS.csv", "SBIN.NS.csv",
             "SUNPHARMA.NS.csv", "TATASTEEL.NS.csv", "WIPRO.NS.csv"]
+
 hpp = {}    # Historical Price Performance
 for i in csv_data:
     with open(i, 'r+', newline='') as f:
         reader = csv.DictReader(f)
+        L = []
         for row in reader:
-            print(row['Open'], row['Close'])
+            L.append(float(row['Close']))
+        L2 = []
+        for j in range(len(L)-1):
+            L2.append((L[j+1] - L[j]) * 100 / L[j])
+        f.close()
+    hpp[i] = L2
+print(hpp)
